d4c6f42c6bc029a2cd5ab174880bba4c7db39858
arifrahmans <arif.ilkom@gmail.com>
2018-04-27 10:20:05 +0700

    Create simple generic Implementation

---
+++ src/com/arif/genericImplement/Player.java
@@ -0,0 +1,13 @@
+package com.arif.genericImplement;
+
+public abstract class Player {
+    private String name;
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String getname(){
+        return name;
+    }
+
+}
